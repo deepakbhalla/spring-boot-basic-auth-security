028d7871a46cf7ab731a119085d4a8bfd23279aa
deepak.dbhalla@gmail.com <deepak.dbhalla@gmail.com>
2023-10-26 23:16:51 +0530

    first commit

---
+++ src/main/java/com/example/security/basicauth/SpringBootBasicAuthSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.security.basicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicAuthSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicAuthSecurityApplication.class, args);
+	}
+
+}
